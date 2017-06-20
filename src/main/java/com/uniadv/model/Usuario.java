726121de5e015226f97baef1aea7db8dd4949611
Iarlen <iarlensilva775@gmail.com>
2017-06-20 14:28:46 -0300

    Finalizando o model Usuario

--- src/main/java/com/uniadv/model/Usuario.java
+++ src/main/java/com/uniadv/model/Usuario.java
@@ -4,7 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity(name = "usuario")
@@ -13,6 +16,64 @@ public abstract class Usuario implements UserDetails {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@NotNull
+	@NotEmpty
+	private String nome;
+
+	@NotNull
+	@NotEmpty
+	private String email;
+
+	private String senha;
+
+	public Usuario() {
+
+	}
+
+	public Usuario(Integer id, String nome, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void encodePassword() {
+		// this.senha = new Md5PasswordEncoder().
+	}
 }

704bf04344b26809c31e2dab1105e798aadd4def
Iarlen <iarlensilva775@gmail.com>
2017-06-20 13:39:53 -0300

    Criando get e set no model cliente

--- src/main/java/com/uniadv/model/Cliente.java
+++ src/main/java/com/uniadv/model/Cliente.java
@@ -70,26 +70,27 @@ public class Cliente {
 
 	@NotNull
 	private String serieCtps;
-	
+
 	@NotNull
 	@Size(max = 2, message = "Campo deve ter {max} caracteres")
 	private String estadoExpedicaoCtps;
-	
+
 	@NotNull
 	private String rua;
-	
+
 	@NotNull
 	private String numero;
-	
+
 	@NotNull
 	private String bairro;
-	
+
 	@NotNull
 	private String complemento;
-	
+
 	@NotNull
+	@Size(min = 5, max = 100, message = "O tamanho deve ser entre {min} e {max}")
 	private String email;
-	
+
 	public Integer getId() {
 		return id;
 	}
@@ -120,6 +121,134 @@ public class Cliente {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public String getNumeroRg() {
+		return numeroRg;
+	}
+
+	public void setNumeroRg(String numeroRg) {
+		this.numeroRg = numeroRg;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public Date getDataExpedicaoRg() {
+		return dataExpedicaoRg;
+	}
+
+	public void setDataExpedicaoRg(Date dataExpedicaoRg) {
+		this.dataExpedicaoRg = dataExpedicaoRg;
+	}
+
+	public String getOrgaoExpedicaoRg() {
+		return orgaoExpedicaoRg;
+	}
+
+	public void setOrgaoExpedicaoRg(String orgaoExpedicaoRg) {
+		this.orgaoExpedicaoRg = orgaoExpedicaoRg;
+	}
+
+	public String getEstadoExpedicaoRg() {
+		return estadoExpedicaoRg;
+	}
+
+	public void setEstadoExpedicaoRg(String estadoExpedicaoRg) {
+		this.estadoExpedicaoRg = estadoExpedicaoRg;
+	}
+
+	public String getNumeroCtps() {
+		return numeroCtps;
+	}
+
+	public void setNumeroCtps(String numeroCtps) {
+		this.numeroCtps = numeroCtps;
+	}
+
+	public String getSerieCtps() {
+		return serieCtps;
+	}
+
+	public void setSerieCtps(String serieCtps) {
+		this.serieCtps = serieCtps;
+	}
+
+	public String getEstadoExpedicaoCtps() {
+		return estadoExpedicaoCtps;
+	}
+
+	public void setEstadoExpedicaoCtps(String estadoExpedicaoCtps) {
+		this.estadoExpedicaoCtps = estadoExpedicaoCtps;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	@Override

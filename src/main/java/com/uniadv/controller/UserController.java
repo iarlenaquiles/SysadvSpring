aa3497060c96f0a41ad7b4ef98bc50dbe0d48a98
Iarlen <iarlensilva775@gmail.com>
2017-06-28 21:08:03 -0300

    Atualizando logout

--- src/main/java/com/uniadv/controller/UserController.java
+++ src/main/java/com/uniadv/controller/UserController.java
@@ -62,10 +62,10 @@ public class UserController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model, String error, String logout) {
 		if (error != null)
-			model.addAttribute("error", "Your username and password is invalid.");
+			model.addAttribute("error", "Usuário ou senha estão inváligos.");
 
 		if (logout != null)
-			model.addAttribute("message", "You have been logged out successfully.");
+			model.addAttribute("message", "Deslogou");
 
 		return "login";
 	}

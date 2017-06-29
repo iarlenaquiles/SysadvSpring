74a4bf1c3f36ed8041f66911b196d8e7bfbc3abe
Iarlen <iarlensilva775@gmail.com>
2017-06-28 21:06:02 -0300

    Criando o form de usuario

--- src/main/java/com/uniadv/controller/UserController.java
+++ src/main/java/com/uniadv/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.uniadv.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.uniadv.model.User;
 import com.uniadv.service.SecurityService;
@@ -29,6 +33,30 @@ public class UserController {
 		model.addAttribute("usuario", new User());
 		model.addAttribute("acao", "/usuarios");
 		return "inserir-usuario";
+	}
+
+	// Salvar usuário no banco
+	@PostMapping("/usuarios")
+	public String addUsuario(@Valid User usuario, BindingResult result, Model model, RedirectAttributes redirect) {
+		if (result.hasErrors()) {
+			model.addAttribute("usuario", usuario);
+			model.addAttribute("acao", "/usuarios");
+			if (usuario.getId() == null) {
+				return "inserir-usuario";
+			} else {
+				return "editar-usuario";
+			}
+		}
+
+		if (usuario.getId() == null) {
+			userService.save(usuario);
+			securityService.autologin(usuario.getUsername(), usuario.getPasswordConfirm());
+			redirect.addFlashAttribute("mensagem", "Usuário inserido com sucesso!");
+		} else {
+			userService.save(usuario);
+			redirect.addFlashAttribute("mensagem", "Usuário atualizado com sucesso!");
+		}
+		return "redirect:/usuarios";
 	}
 
 	@RequestMapping(value = "/registro", method = RequestMethod.POST)

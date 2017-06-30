d09def298bf7d035dbc0c92a7f7251e4c88dcf00
Iarlen <iarlensilva775@gmail.com>
2017-06-30 12:35:29 -0300

    Criando o script sql

---
+++ src/main/resources/script.sql
@@ -0,0 +1,45 @@
+INSERT INTO clientes(
+            id, nome, bairro, cep, cidade, complemento, cpf, data, data_expedicao_rg, 
+            email, estado, estado_civil, estado_expedicao_ctps, estado_expedicao_rg, 
+            nacionalidade, naturalidade, nome_mae, nome_pai, numero, 
+            numero_ctps, numero_rg, observacao, orgao_expedicao_rg, profissao, 
+            rua, serie_ctps, telefone)
+    VALUES (3,'Roberta Andrade', 'Centro', '63900-049', 'Quixadá', 'perto do ponto do baião','111.111.111-11', '1990-05-14 00:00:00', '1990-05-14 00:00:00', 
+            'teste@gmail.com', 'CE', 'Solteiro', 'CE', 'CE', 
+            'Brasileira', 'Fortaleza', 'Teste','Teste','123',
+            '123', '123', 'teste', 'ssp', 'estudante','Jose Viana de Sousa', 
+            '123', '(85) 5050-0595');
+            
+            INSERT INTO clientes(
+            id, nome, bairro, cep, cidade, complemento, cpf, data, data_expedicao_rg, 
+            email, estado, estado_civil, estado_expedicao_ctps, estado_expedicao_rg, 
+            nacionalidade, naturalidade, nome_mae, nome_pai, numero, 
+            numero_ctps, numero_rg, observacao, orgao_expedicao_rg, profissao, 
+            rua, serie_ctps, telefone)
+    VALUES (3,'Bruno Fernandes', 'Centro', '63900-049', 'Quixadá', 'perto do ponto do baião','111.111.111-11', '1990-05-14 00:00:00', '1990-05-14 00:00:00', 
+            'teste@gmail.com', 'CE', 'Solteiro', 'CE', 'CE', 
+            'Brasileira', 'Fortaleza', 'Teste','Teste','123',
+            '123', '123', 'teste', 'ssp', 'estudante','Jose Viana de Sousa', 
+            '123', '(85) 5050-0595');
+            
+            
+            
+INSERT INTO processo(
+            id, acao, ano_processo, cidade_comarca, estado_comarca, forum, 
+            honorario, numero_processo, observacao, porcentagem_causa, procedimento, 
+            tipo_justica, tipo_processo, vara_subsecao, cliente_id)
+    VALUES (1, 'Ação Penal', '2017', 'Quixadá', 'CE', 'Forum', 
+            '"R$ 500,00"', '123321', 'teste', '012%', 'Procedimento Sumário', 
+            'Municipal', 'Administrativo', '10ª', 2);
+            
+            
+INSERT INTO processo(
+            id, acao, ano_processo, cidade_comarca, estado_comarca, forum, 
+            honorario, numero_processo, observacao, porcentagem_causa, procedimento, 
+            tipo_justica, tipo_processo, vara_subsecao, cliente_id)
+    VALUES (1, 'Ação Penal', '2017', 'Quixadá', 'CE', 'Forum', 
+            '"R$ 500,00"', '123321', 'teste', '012%', 'Procedimento Sumário', 
+            'Municipal', 'Administrativo', '10ª', 2);
+            
+            
+            

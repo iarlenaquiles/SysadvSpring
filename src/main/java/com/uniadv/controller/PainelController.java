0372810d0c61d0b100922feeca50dc1c8c5716da
Iarlen <iarlensilva775@gmail.com>
2017-06-30 11:27:14 -0300

    Melhorando o painel

--- src/main/java/com/uniadv/controller/PainelController.java
+++ src/main/java/com/uniadv/controller/PainelController.java
@@ -1,30 +1,13 @@
 package com.uniadv.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.uniadv.repository.ClienteRepository;
-import com.uniadv.repository.ProcessoRepository;
-import com.uniadv.repository.UserRepository;
 
 @Controller
 public class PainelController {
-	@Autowired
-	ClienteRepository clienteRepository;
-	
-	@Autowired
-	ProcessoRepository processoRepository;
-	
-	@Autowired
-	UserRepository userRepository;
 
 	@RequestMapping("/")
-	public String home(Model model) {
-		model.addAttribute("qtd_cliente", clienteRepository.count());
-		model.addAttribute("qtd_processo", processoRepository.count());
-		model.addAttribute("qtd_user", userRepository.count());
+	public String home() {
 		return "painel";
 	}
 }

a368465bec8c8b965371c45a67a9ead06edc6e9c
Iarlen <iarlensilva775@gmail.com>
2017-06-29 22:06:51 -0300

    Cliando os contadores dos clientes e dos processos

--- src/main/java/com/uniadv/controller/PainelController.java
+++ src/main/java/com/uniadv/controller/PainelController.java
@@ -1,14 +1,25 @@
 package com.uniadv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.uniadv.repository.ClienteRepository;
+import com.uniadv.repository.ProcessoRepository;
+
 @Controller
 public class PainelController {
+	@Autowired
+	ClienteRepository clienteRepository;
 	
+	@Autowired
+	ProcessoRepository processoRepository;
+
 	@RequestMapping("/")
 	public String home(Model model) {
+		model.addAttribute("qtd_cliente", clienteRepository.count());
+		model.addAttribute("qtd_processo", processoRepository.count());
 		return "painel";
 	}
 }

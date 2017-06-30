843c88d9a78c08c0cd5d807b302bd0a942858841
Iarlen <iarlensilva775@gmail.com>
2017-06-29 22:49:08 -0300

    Melhorando o cliente

--- src/main/java/com/uniadv/controller/ClienteController.java
+++ src/main/java/com/uniadv/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,7 +48,6 @@ public class ClienteController {
 		Cliente c = clienteRepository.findOne(id);
 		model.addAttribute("cliente", c);
 		model.addAttribute("acao", "/clientes");
-
 		return "editar-cliente";
 	}
 

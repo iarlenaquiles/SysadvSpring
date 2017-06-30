9f9e779c0be131974765aa3a060db46ab6efa8b2
Iarlen <iarlensilva775@gmail.com>
2017-06-30 11:28:00 -0300

    Criando a contagem dos usuarios

--- src/main/java/com/uniadv/controller/UserController.java
+++ src/main/java/com/uniadv/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.uniadv.model.User;
@@ -92,6 +93,11 @@ public class UserController {
 			model.addAttribute("message", "Deslogou");
 
 		return "login";
+	}
+	
+	@RequestMapping("/qtdUsuarios")
+	public @ResponseBody Long qtdClientes() {
+		return userService.count();
 	}
 
 }

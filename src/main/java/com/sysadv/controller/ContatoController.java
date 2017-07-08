46ab4c789983dc88c2ae09fd097b1f341eddfb52
Iarlen <iarlensilva775@gmail.com>
2017-07-08 19:27:38 -0300

    Criando o controller de contato

--- src/main/java/com/sysadv/controller/ContatoController.java
+++ src/main/java/com/sysadv/controller/ContatoController.java
@@ -1,8 +1,15 @@
 package com.sysadv.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.sysadv.model.Contato;
 import com.sysadv.repository.ContatoRepository;
 
 @Controller
@@ -10,8 +17,20 @@ public class ContatoController {
 
 	@Autowired
 	private ContatoRepository contatoRepository;
-	
-	public String addContato(){
-		return "";
+
+	@PostMapping("/contatos")
+	public String addContato(@Valid Contato contato, BindingResult result, Model model, RedirectAttributes redirect) {
+		if (result.hasErrors()) {
+			model.addAttribute("contato", contato);
+			model.addAttribute("acao", "/contatos");
+			return "contato";
+		}
+
+		if (contato.getId() == null) {
+			contatoRepository.save(contato);
+			redirect.addFlashAttribute("mensagem", "Mensagem enviada com sucesso!");
+		}
+		
+		return "redirect:/sysadv/contato";
 	}
 }

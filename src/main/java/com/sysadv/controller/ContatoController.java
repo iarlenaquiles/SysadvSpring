5f1ae4b584153f2583bb450f884e2cd1c118f8e0
Iarlen <iarlensilva775@gmail.com>
2017-07-08 19:09:40 -0300

    Criando o controller de contato

--- src/main/java/com/sysadv/controller/ContatoController.java
+++ src/main/java/com/sysadv/controller/ContatoController.java
@@ -1,8 +1,17 @@
 package com.sysadv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.sysadv.repository.ContatoRepository;
 
 @Controller
 public class ContatoController {
 
+	@Autowired
+	private ContatoRepository contatoRepository;
+	
+	public String addContato(){
+		return "";
+	}
 }

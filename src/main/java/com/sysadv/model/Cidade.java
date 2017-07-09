43afbddc6506cfc48e6cf2c72e8b8d4ff878b079
Iarlen <iarlensilva775@gmail.com>
2017-07-08 22:59:57 -0300

    Criando o model cidade

---
+++ src/main/java/com/sysadv/model/Cidade.java
@@ -0,0 +1,45 @@
+package com.sysadv.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity(name = "cidade")
+public class Cidade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	@NotNull
+	@OneToOne
+	private Estado estado;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+}

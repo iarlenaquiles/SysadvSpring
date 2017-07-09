6b2ee80598074867cb7fcc2bd8acf9f7fce448c9
Iarlen <iarlensilva775@gmail.com>
2017-07-09 01:00:23 -0300

    removendo metodo

--- src/main/java/com/sysadv/repository/CidadeRepository.java
+++ src/main/java/com/sysadv/repository/CidadeRepository.java
@@ -1,10 +1,6 @@
 package com.sysadv.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sysadv.model.Cidade;
@@ -12,6 +8,4 @@ import com.sysadv.model.Cidade;
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 
-	@Query("select c.id, c.nome from cidade c, estado e  where e.id=:id")
-	public List<Cidade> getListaCidadeByEstado(@Param("id") Integer idEstado);
 }

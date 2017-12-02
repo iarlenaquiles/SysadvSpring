cd20b6a1747280c0b48d04f65d0dff1019919135
Iarlen <iarlensilva775@gmail.com>
2017-12-02 18:29:01 -0300

    Criando a service do contato e incluindo o css do taginput

---
+++ src/main/java/com/sysadv/service/ContatoService.java
@@ -0,0 +1,11 @@
+package com.sysadv.service;
+
+import java.util.List;
+
+import com.sysadv.model.Contato;
+
+public interface ContatoService {
+	void save(Contato contato);
+
+	List<Contato> getLista();
+}
---
+++ src/main/java/com/sysadv/service/ContatoServiceImpl.java
@@ -0,0 +1,33 @@
+package com.sysadv.service;
+
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.sysadv.model.Contato;
+
+public class ContatoServiceImpl implements ContatoService {
+	
+	MongoClient mongoClient = new MongoClient();
+	MongoDatabase db = mongoClient.getDatabase("sysadv_contato");
+	MongoCollection<Document> collectionContato = db.getCollection("contato");
+	
+	@Override
+	public void save(Contato contato) {
+		Document doc = new Document();
+		doc.append("nome", contato.getNome());
+		doc.append("email", contato.getEmail());
+		doc.append("telefones", contato.getTelefone());
+		collectionContato.insertOne(doc);
+	}
+
+	@Override
+	public List<Contato> getLista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

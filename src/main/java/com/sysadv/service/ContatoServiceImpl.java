d583a174b01b08f7b5c93afe095869d1f4d24fb2
Iarlen <iarlensilva775@gmail.com>
2017-12-02 18:34:27 -0300

    Criando a implementação do serviço do contato

--- src/main/java/com/sysadv/service/ContatoServiceImpl.java
+++ src/main/java/com/sysadv/service/ContatoServiceImpl.java
@@ -3,24 +3,28 @@ package com.sysadv.service;
 import java.util.List;
 
 import org.bson.Document;
+import org.springframework.stereotype.Service;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.sysadv.model.Contato;
 
+@Service
 public class ContatoServiceImpl implements ContatoService {
-	
+
 	MongoClient mongoClient = new MongoClient();
 	MongoDatabase db = mongoClient.getDatabase("sysadv_contato");
 	MongoCollection<Document> collectionContato = db.getCollection("contato");
-	
+
 	@Override
 	public void save(Contato contato) {
 		Document doc = new Document();
 		doc.append("nome", contato.getNome());
 		doc.append("email", contato.getEmail());
 		doc.append("telefones", contato.getTelefone());
+		doc.append("mensagem", contato.getMensagem());
+
 		collectionContato.insertOne(doc);
 	}
 

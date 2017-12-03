5459c581b2f827f335f6a0c2730ddf8c037a6252
Iarlen <iarlensilva775@gmail.com>
2017-12-03 20:58:01 -0300

    Implementando o método getUser pelo nome

--- src/main/java/com/sysadv/cache/UsuarioCache.java
+++ src/main/java/com/sysadv/cache/UsuarioCache.java
@@ -1,8 +1,11 @@
 package com.sysadv.cache;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import com.sysadv.model.User;
@@ -13,14 +16,19 @@ public class UsuarioCache {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	static final Logger logger = Logger.getLogger(UsuarioCache.class);
-	
+
 	@Cacheable(value = "usuarioCache", key = "#nome")
 	public User getUsuario(String nome) {
 		logger.info("[CACHE] Realizando consulta pelo usuario com nome: " + nome);
 		return userRepository.findByUsername(nome);
 	}
 
+	@Cacheable(value = "usuariosCache", key = "#nome")
+	public List<User> getAll() {
+		logger.info("[CACHE] Listando todos os usuarios");
+		return userRepository.findAll(new Sort(new Sort.Order(Sort.Direction.ASC, "username")));
+	}
 
 }

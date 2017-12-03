4361dfd3bb5fdac76396bdb65d034d9d003c17c4
Iarlen <iarlensilva775@gmail.com>
2017-12-02 21:14:48 -0300

    Criando o departamento

--- src/main/java/com/sysadv/model/Departamento.java
+++ src/main/java/com/sysadv/model/Departamento.java
@@ -5,17 +5,14 @@ import org.bson.types.ObjectId;
 public class Departamento {
 
 	private ObjectId idMongo;
-	private Double id;
 	private String nome;
 
 	public Departamento() {
 
 	}
 
-	public Departamento(ObjectId idMongo, Double id, String nome) {
-		super();
+	public Departamento(ObjectId idMongo, String nome) {
 		this.idMongo = idMongo;
-		this.id = id;
 		this.nome = nome;
 	}
 
@@ -25,14 +22,6 @@ public class Departamento {
 
 	public void setIdMongo(ObjectId idMongo) {
 		this.idMongo = idMongo;
-	}
-
-	public Double getId() {
-		return id;
-	}
-
-	public void setId(Double id) {
-		this.id = id;
 	}
 
 	public String getNome() {

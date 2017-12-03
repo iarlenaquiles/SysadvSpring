9e541f3a708d071ef4055039c75f75055ccd918d
Iarlen <iarlensilva775@gmail.com>
2017-12-03 17:47:50 -0300

    Criando o metodo getallProcessos

--- src/main/java/com/sysadv/cache/ProcessoCache.java
+++ src/main/java/com/sysadv/cache/ProcessoCache.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import com.sysadv.model.Processo;
@@ -17,6 +18,7 @@ public class ProcessoCache {
 	
 	static final Logger logger = Logger.getLogger(ProcessoCache.class);
 	
+	@Cacheable(value = "processoCache")
 	public List<Processo> getAll(){
 		logger.info("[CACHE] Listando todos os processos!");
 		return processoRepository.findAll();

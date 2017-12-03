ae7b2066f8b1b9dd8d73532956884e4bfe4cd743
Iarlen <iarlensilva775@gmail.com>
2017-12-03 17:40:27 -0300

    Criando os métodos do cache

---
+++ src/main/java/com/sysadv/cache/ClienteCache.java
@@ -0,0 +1,39 @@
+package com.sysadv.cache;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import com.sysadv.model.Cliente;
+import com.sysadv.repository.ClienteRepository;
+
+@Component
+public class ClienteCache {
+
+	@Autowired
+	ClienteRepository clienteRepository;
+
+	static final Logger logger = Logger.getLogger(ClienteCache.class);
+
+	@Cacheable(value = "clienteCache")
+	public Cliente getCliente(Integer id) {
+		logger.info("[CACHE] Realizando consulta pelo cliente com id: " + id);
+		return clienteRepository.findOne(id);
+	}
+
+	@Cacheable(value = "ClientesCache")
+	public List<Cliente> getAllClientes() {
+		logger.info("[CACHE] De todos os clientes");
+		return clienteRepository.findAll(new Sort(new Sort.Order(Sort.Direction.ASC, "nome")));
+	}
+
+	@Cacheable(value = "qtdClientes")
+	public Long qtdClientes() {
+		logger.info("[CACHE] Quantidade de clientes");
+		return clienteRepository.count();
+	}
+}

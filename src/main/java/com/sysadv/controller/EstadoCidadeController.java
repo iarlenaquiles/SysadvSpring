cc8416690755c99c634ab1895f4c2643c1285845
Iarlen <iarlensilva775@gmail.com>
2017-12-03 18:09:46 -0300

    Chamando no controller

--- src/main/java/com/sysadv/controller/EstadoCidadeController.java
+++ src/main/java/com/sysadv/controller/EstadoCidadeController.java
@@ -3,20 +3,19 @@ package com.sysadv.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sysadv.cache.CidadeCache;
 import com.sysadv.cache.EstadoCache;
 import com.sysadv.model.Cidade;
 import com.sysadv.model.Estado;
-import com.sysadv.repository.CidadeRepository;
 
 @RestController
 public class EstadoCidadeController {
 
 	@Autowired
-	private CidadeRepository cidadeRepository;
+	private CidadeCache cidadeCache;
 	
 	@Autowired
 	private EstadoCache estadoCache;
@@ -29,7 +28,7 @@ public class EstadoCidadeController {
 
 	@RequestMapping("/getCidade")
 	public List<Cidade> getCidade() {
-		List<Cidade> lista = cidadeRepository.findAll(new Sort(new Sort.Order(Sort.Direction.ASC, "nome")));
+		List<Cidade> lista = cidadeCache.getAllCidades();
 		return lista;
 	}
 

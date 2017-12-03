67331d5771e6e5cf146094ee8d847fefca4ecca2
Iarlen <iarlensilva775@gmail.com>
2017-12-03 18:10:02 -0300

    Criando o método

--- src/main/java/com/sysadv/cache/CidadeCache.java
+++ src/main/java/com/sysadv/cache/CidadeCache.java
@@ -1,5 +1,26 @@
 package com.sysadv.cache;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import com.sysadv.model.Cidade;
+import com.sysadv.repository.CidadeRepository;
+
+@Component
 public class CidadeCache {
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	static final Logger logger = Logger.getLogger(CidadeCache.class);
+
+	@Cacheable(value = "CidadesCache")
+	public List<Cidade> getAllCidades() {
+		logger.info("[CACHE] De todos as cidades");
+		return cidadeRepository.findAll(new Sort(new Sort.Order(Sort.Direction.ASC, "nome")));
+	}
 
 }

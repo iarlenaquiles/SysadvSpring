926f06311861e6827083286324e724437d2ee022
Iarlen <iarlensilva775@gmail.com>
2017-12-03 17:44:30 -0300

    Criando o listar todos os processos

--- src/main/java/com/sysadv/cache/ProcessoCache.java
+++ src/main/java/com/sysadv/cache/ProcessoCache.java
@@ -1,5 +1,25 @@
 package com.sysadv.cache;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.sysadv.model.Processo;
+import com.sysadv.repository.ProcessoRepository;
+
+@Component
 public class ProcessoCache {
+	
+	@Autowired
+	ProcessoRepository processoRepository;
+	
+	static final Logger logger = Logger.getLogger(ProcessoCache.class);
+	
+	public List<Processo> getAll(){
+		logger.info("[CACHE] Listando todos os processos!");
+		return processoRepository.findAll();
+	}
 
 }

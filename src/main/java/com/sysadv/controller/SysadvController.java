e6b5d1c4ce6b1a5ef6600516e23ebcbf2f00d7da
Iarlen <iarlensilva775@gmail.com>
2017-12-02 21:14:00 -0300

    Inserindo o departamento na listagem

--- src/main/java/com/sysadv/controller/SysadvController.java
+++ src/main/java/com/sysadv/controller/SysadvController.java
@@ -1,14 +1,22 @@
 package com.sysadv.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sysadv.model.Contato;
+import com.sysadv.model.Departamento;
+import com.sysadv.service.DepartamentoService;
 
 @Controller
 public class SysadvController {
 
+	@Autowired
+	private DepartamentoService departamentoRepository;
+	
 	@RequestMapping("/sysadv")
 	public String uniadv(){
 		return "sysadv";
@@ -21,8 +29,10 @@ public class SysadvController {
 	
 	@RequestMapping("/sysadv/contato")
 	public String contato(Model model){
+		List<Departamento> depts = departamentoRepository.getLista();
 		model.addAttribute("contato", new Contato());
 		model.addAttribute("acao", "/contatos");
+		model.addAttribute("departamentos", depts);
 		return "contato";
 	}
 }

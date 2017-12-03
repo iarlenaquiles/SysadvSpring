8e98fe7ceee1065cd2c8c9995d713dec1580cfff
Iarlen <iarlensilva775@gmail.com>
2017-12-02 21:10:17 -0300

    Criando o serviceimpl do departamento

---
+++ src/main/java/com/sysadv/service/DepartamentoServiceImpl.java
@@ -0,0 +1,54 @@
+package com.sysadv.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.sysadv.model.Departamento;
+
+@Service
+public class DepartamentoServiceImpl implements DepartamentoService {
+
+	MongoClient mongoClient = new MongoClient();
+	MongoDatabase db = mongoClient.getDatabase("sysadv_contato");
+	MongoCollection<Document> collectionDepartamento = db.getCollection("departamento");
+
+	@Override
+	public void save(Departamento departamento) {
+		Document doc = new Document();
+		doc.append("nome", departamento.getNome());
+
+		collectionDepartamento.insertOne(doc);
+	}
+
+	@Override
+	public List<Departamento> getLista() {
+		List<Departamento> departamentos = new ArrayList<Departamento>();
+
+		for (Document doc : collectionDepartamento.find()) {
+			Departamento departamento = new Departamento();
+			departamento.setIdMongo(doc.getObjectId("_id"));
+			departamento.setNome(doc.getString("nome"));
+
+			departamentos.add(departamento);
+		}
+		return departamentos;
+	}
+
+	@Override
+	public void remove(String id) {
+		collectionDepartamento.deleteOne(new Document("_id", new ObjectId(id)));
+	}
+
+	@Override
+	public Long count() {
+		return collectionDepartamento.count();
+	}
+
+}

f53f128ba0c4ce2b670ef85c7c5b00ecd035a2b6
Iarlen <iarlensilva775@gmail.com>
2017-12-03 17:38:37 -0300

    Implementanto o serializable

--- src/main/java/com/sysadv/model/Cidade.java
+++ src/main/java/com/sysadv/model/Cidade.java
@@ -1,5 +1,7 @@
 package com.sysadv.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,8 +10,9 @@ import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity(name = "cidade")
-public class Cidade {
+public class Cidade implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;

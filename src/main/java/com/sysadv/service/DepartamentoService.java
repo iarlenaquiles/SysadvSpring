df5445ccec9388473319f893ef6297dd4cb1c048
Iarlen <iarlensilva775@gmail.com>
2017-12-05 11:19:05 -0300

    Melhorando a service

--- src/main/java/com/sysadv/service/DepartamentoService.java
+++ src/main/java/com/sysadv/service/DepartamentoService.java
@@ -6,7 +6,7 @@ import com.sysadv.model.Departamento;
 
 public interface DepartamentoService {
 	void save(Departamento departamento);
-	
+
 	void update(Departamento departamento);
 
 	List<Departamento> getLista();
